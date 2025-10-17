d4c94bb8b3b3ecaadd079112cf73738e99f865dc
narellazorzon <narellazorzon@gmail.com>
2025-10-17 07:51:19 -0300

    agregue google analitics

--- partials/header.php
+++ partials/header.php
@@ -36,6 +36,15 @@
   
   <link rel="stylesheet" href="<?= $basePath ?>assets/css/style_comp.css?v=<?= time() ?>">
   
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-4PM9446E04"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-4PM9446E04');
+  </script>
+  
 </head>
 <body>
 <header>
